da168d4f831e1b0960bdd5c3db99e5187f6d32d8
Patrick Park <parkpatrickj@gmail.com>
2020-07-05 01:20:06 -0700

    update main

--- src/controller.py
+++ src/controller.py
@@ -34,6 +34,33 @@ class Controller():
 
 	data_listen = Process(target=data_handler)
 	def data_handler(self):
+		while True:
+			header, data = serial_connection.receive()
+			(PS, PL, DID, VER, PN, CH, PT) = header
+			if not data and not header:
+				print('error in transmission')
+			if (PT == 1):
+				print("envelope packet\n")
+			# numerics packet sent once a second
+			elif (PT == 2):
+				print('numerics packet')
+				num_packet = packet_decoder.decode(header, data)
+				print(num_packet)
+				tcd_network_post(num_packet, dest_ip)
+			# messages packet
+			elif (PT == 3):
+				print("message packet")
+				print(packet_decoder.decode(header, data))
+			# error packet
+			elif (PT == 4):
+				print("error packet")
+				print(packet_decoder.decode(header, data))
+
+	
+
+
+
+
 
 
 
@@ -50,31 +77,31 @@ packet_decoder = PacketDecoder.getInstance()
 
 
 def tcd_network_post(payload, dest_ip):
-    print("called tcd_network_post")
-    payload = {'TCDMonitor': payload}
-    print(payload)
-    r = requests.put(dest_ip, data=json.dumps(payload))
-    print(r.text)
+	print("called tcd_network_post")
+	payload = {'TCDMonitor': payload}
+	print(payload)
+	r = requests.put(dest_ip, data=json.dumps(payload))
+	print(r.text)
 
 
 while True:
-    header, data = serial_connection.receive()
-    (PS, PL, DID, VER, PN, CH, PT) = header
-    if not data and not header:
-        print('error in transmission')
-    if (PT == 1):
-        print("envelope packet\n")
-    # numerics packet sent once a second
-    elif (PT == 2):
-        print('numerics packet')
-        num_packet = packet_decoder.decode(header, data)
-        print(num_packet)
-        tcd_network_post(num_packet, dest_ip)
-    # messages packet
-    elif (PT == 3):
-        print("message packet")
-        print(packet_decoder.decode(header, data))
-    # error packet
-    elif (PT == 4):
-        print("error packet")
-        print(packet_decoder.decode(header, data))
+	header, data = serial_connection.receive()
+	(PS, PL, DID, VER, PN, CH, PT) = header
+	if not data and not header:
+		print('error in transmission')
+	if (PT == 1):
+		print("envelope packet\n")
+	# numerics packet sent once a second
+	elif (PT == 2):
+		print('numerics packet')
+		num_packet = packet_decoder.decode(header, data)
+		print(num_packet)
+		tcd_network_post(num_packet, dest_ip)
+	# messages packet
+	elif (PT == 3):
+		print("message packet")
+		print(packet_decoder.decode(header, data))
+	# error packet
+	elif (PT == 4):
+		print("error packet")
+		print(packet_decoder.decode(header, data))
--- src/main.py
+++ src/main.py
@@ -1,18 +1,51 @@
 import argparse
+from controller import Controller
+import signal
+
+### Get cmd line arguments from user ###
 
 parser = argparse.ArgumentParser(description='Program controller')
 
 parser.add_argument('-b', '--baud', default='12400')
 parser.add_argument('-o', '--outfile')
 parser.add_argument('-w', '--web_out')
+parser.add_argument('-p', '--port', default='/dev/ttyUSB0')
 
 args = parser.parse_args()
-
-print(args.baud)
-print(args.outfile)
-print(args.web_out)
 
 if args.outfile == None and args.web_out == None:
     print('specify at least one output destination')
     exit()
 
+arg_baud = args.baud
+arg_outfile = args.outfile
+arg_web_out = args.web_out
+arg_port = args.port
+
+
+### functions to start and stop controller ###
+
+def start_controller(self, arg_baud, arg_outfile, arg_web_out, arg_port):
+    main_controller = Controller(arg_baud, arg_outfile, arg_web_out, arg_port)
+    main_controller.start()
+
+def stop_controller(signal, frame):
+    print('Ctrl-C detected. Stopping script')
+    main_controller.stop()
+    exit(0)
+
+
+### execute stop_controller when ctrl-c (signal interrupt) is detected ###
+
+signal.signal(signal.SIGINT, stop_controller)
+
+
+### start the controller ###
+
+start_controller(arg_baud, arg_outfile, arg_web_out, arg_port)
+
+
+### run forever ###
+
+while True:
+    pass
