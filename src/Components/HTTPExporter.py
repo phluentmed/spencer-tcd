6b6c7310e21ffc0ffad07351ecc92c1c221bfa0b
Farhan Ahmed <fahmed96@uwo.ca>
2020-07-09 00:37:20 -0400

    Addressed some comments

--- src/Components/HTTPExporter.py
+++ src/Components/HTTPExporter.py
@@ -1,16 +1,15 @@
 from Interfaces.DataExporter import DataExport
 from Utilities.EventScheduler import EventScheduler
 
-import json #TODO add in requirements for pip install
-import requests #TODO add in requirements for pip install
+import json 
+import requests 
 
 class HTTPExporter(DataExporter):
 
 	def __init__(self, host_address):
 		self._host_address = host_address
-		self_event_scheduler = EventScheduler("http_thread")
+		self._event_scheduler = EventScheduler("http_thread")
 		self._is_running = False
-		self._data_buffer = []
 
 	def start(self):
 		#TODO: have some retry logic on connect
@@ -30,27 +29,18 @@ class HTTPExporter(DataExporter):
 	
 	def export(self, data, result_handler):
 		if self._is_running:
-			self_event_scheduler.enter(0,
-									   1,
-									   self._export_dispatched,
-									   (data, result_handler))
+			self._event_scheduler.enter(0, 
+										1,
+										self._export_dispatched, 
+										(data, result_handler))
 		#TODO add helpful message for feedback
 		return -1
 	
-	def _export_dispatched(self, data, result_handler, flush_buffer=False):
-		if data:
-			self._data_buffer.append(data)
+	def _export_dispatched(self, data, result_handler):
 		#TODO add negling 
-		if self._data_buffer:
+		if data:
 			response = requests.put(self._host_address, 
 									data=json.dumps(data))
 			if response.status_code != 200:
 				print("Error sending data to " + self._host_address +
 					  " with error code" + response.status_code)
-		if flush_buffer and self._data_buffer:
-				response = requests.put(self._host_address, 
-									data=json.dumps(data))
-			if response.status_code != 200:
-				print("Error sending data to " + self._host_address +
-					  " with error code" + response.status_code)
-		
