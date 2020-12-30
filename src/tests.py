ab86c838ef666215997d005247721bb09b8e8ce5
dil-mchaina <mmchain@uwo.ca>
2020-12-29 20:11:15 -0500

    Update tests.py

--- src/tests.py
+++ src/tests.py
@@ -3,5 +3,7 @@ import unittest
 unit_tests = unittest.TestLoader().discover('Tests/UnitTests', '*Tests.py')
 integration_tests = unittest.TestLoader().discover('Tests/IntegrationTests',
                                                    '*Tests.py')
-unittest.TextTestRunner().run(unit_tests)
-unittest.TextTestRunner().run(integration_tests)
+u_result = unittest.TextTestRunner().run(unit_tests)
+i_result = unittest.TextTestRunner().run(integration_tests)
+
+sys.exit(not (u_result.wasSuccessful() and i_result.wasSuccessful()))
